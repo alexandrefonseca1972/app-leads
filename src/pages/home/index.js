58528e5e5af59454bbf4cbb3437195fbe40dc238
Alexandre Fonseca <alexandre.fonseca@live.com>
2022-04-19 12:42:16 -0300

    makk 5

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from 'react-router-dom';
+import "./styles.css";
+
 
 export default function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Link to="/cad">Cadastrar</Link>
+            <div className="arealink">
+                <Link to="/cad">Cadastrar</Link>
+                <Link to="/list">Listagem</Link>
+            </div>
         </div>
     )
 }
--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,9 +1,35 @@
 import React from "react";
+import api from '../../api';
 
 export default function List() {
+
+    const [listagem, setListagem] = React.useState([]);
+    async function HandleList() {
+        const dt = await api.get('/lead/list');
+        setListagem(dt.data);
+    }
+
+    React.useEffect(() => {
+        HandleList();
+    }, [])
     return (
-        <div>
-            <h1>List</h1>
-        </div>
+        <html lang="pt_BR">
+            <head>
+                <meta charset="UTF-8" />
+                <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <title>Listagem de Leads</title>
+            </head>
+            <body>
+                <h1>Listagem</h1>
+                {listagem.map((item) => {
+                    return (
+                        <p key={item._id}><strong>Nome</strong> : {item.nome} - <strong>Curso</strong> : {item.curso}</p>
+                    )
+                })}
+
+
+            </body>
+        </html>
     )
 }
