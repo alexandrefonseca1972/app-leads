211ed0fa57d6e5a55260d4e9eeea5d348c1b81cf
Alexandre Fonseca <alexandre.fonseca@live.com>
2022-04-19 20:27:01 -0300

    makk 8

--- src/pages/list/index.js
+++ src/pages/list/index.js
@@ -1,8 +1,14 @@
 import React from "react";
 import api from '../../api';
+import { useNavigate, Link } from 'react-router-dom';
+import { FiEdit, FiTrash2, FiCornerDownLeft } from 'react-icons/fi'
+import "./styles.css";
+
+
 
 export default function List() {
 
+    const navigate = useNavigate();
     const [listagem, setListagem] = React.useState([]);
     async function HandleList() {
         const dt = await api.get('/lead/list');
@@ -22,13 +28,36 @@ export default function List() {
             </head>
             <body>
                 <h1>Listagem</h1>
-                {listagem.map((item) => {
-                    return (
-                        <p key={item._id}><strong>Nome</strong> : {item.nome} - <strong>Curso</strong> : {item.curso}</p>
-                    )
-                })}
+                <div className="tabArea">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Nome</th>
+                                <th>Curso</th>
+                                <th>Whatsapp</th>
+                                <th>Excluir</th>
+                                <th>Editar</th>
 
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {listagem.map((i) =>
+                                <tr key={i._id}>
+                                    <td>{i.nome}</td>
+                                    <td>{i.curso}</td>
+                                    <td>{i.whatsapp}</td>
+                                    <td><FiTrash2 className="Iconapp" size={16} color={"rgb(231, 6, 51"} onClick={() => { navigate("/home") }} /></td>
+                                    <td><FiEdit className="Iconapp" size={16} color={"rgb(231, 6, 51"} onClick={() => { navigate("/home") }} /></td>
+                                </tr>
+                            )}
 
+                        </tbody>
+                    </table>
+                </div>
+                <div className="areareturn">
+                    <FiCornerDownLeft className="Iconapp" size={16} color={"rgb(231, 6, 51"} />
+                    <Link to={"/"} style={{ color: "rgb(231, 6, 51)" }}>Retornar</Link>
+                </div>
             </body>
         </html>
     )
